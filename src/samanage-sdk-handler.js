e1f0fbf9a39433f9bb8e638ab63a239e535cf1f6
Devin Janus <devinjanus@DJanus-MBA-8GB.local>
2017-05-23 15:27:59 -0400

    small fixes

--- src/samanage-sdk-handler.js
+++ src/samanage-sdk-handler.js
@@ -1,27 +1,18 @@
 import util from 'util'
 import _ from 'lodash'
-import Promise from 'bluebird'
+// import Promise from 'bluebird'
 import samanage from './samanage-api.js'
-import config from './config.js'
-import https from 'https'
 
-const options = config('SAMANAGE_OPTIONS')
 const ActionsSdkApp = require('actions-on-google').ActionsSdkApp
-
-// consts for intent map
-const GOOGLE_ASSISTANT_WELCOME = 'input.welcome'
-const SINGLE_RETURN_NO_CONTEXT = 'singleReturn.noContext'
-const GET_LATEST = 'getLatest'
 
 const welcomeIntent = (assistant) => {
   console.log('** inside welcome case **')
   assistant.ask('What can I do for you? If you are not totally sure what to do, just say: I need help')
 }
 
-
 const singleReturnNCIntent = (assistant, cb) => {
   console.log('** inside single return [nc] case **')
-  let type = assistant.getArgument('record-type')
+  const type = assistant.getArgument('record-type')
   let recordType = type
   if (!_.endsWith(recordType, 's')) recordType += 's'
   const caseNumber = assistant.getArgument('caseNumber')
@@ -30,10 +21,10 @@ const singleReturnNCIntent = (assistant, cb) => {
     console.log(`--> record ${util.inspect(record.number)} retrieved`)
     let text = `I'm sorry, I was unable to retrieve any information on that ${recordType}`
     if (record !== 'none' || null || undefined) text = `The ${returnType} of ${type} ${caseNumber} is ${record[returnType]}`
-    return cb(null, text)
+    cb(null, text)
   })
   .catch((err) => {
-    return cb(err, null)
+    cb(err, null)
   })
 }
 
@@ -44,7 +35,8 @@ const singleReturnNCIntent = (assistant, cb) => {
 //   return samanage.getLatest(recordType).then((record) => {
 //     console.log(`--> record ${util.inspect(record)} retrieved`)
 //     let text = `I'm sorry, I was unable to retrieve any information on that ${recordType}`
-//     if (record !== 'none' || null || undefined) text = `The latest ${recordType} is ${record.number}, ${record.name}. Would you like more information on that?`
+//     if (record !== 'none' || null || undefined) text = `The latest ${recordType} is ${record.number}, ${record.name}.
+//       Would you like more information on that?`
 //     console.log(`text output: ${text}`)
 //     return cb(null, text)
 //   })
